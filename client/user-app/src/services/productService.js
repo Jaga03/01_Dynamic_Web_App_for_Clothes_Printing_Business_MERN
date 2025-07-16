a2377f549c369d5b2fd14da35663bf8446a8c4a0
Amit Kumar Pati <121998394+sanuamit@users.noreply.github.com>
2025-07-16 15:55:57 +0530

    Create productService.js

---
+++ client/user-app/src/services/productService.js
@@ -0,0 +1,12 @@
+// services/productService.js
+import axios from 'axios';
+
+export const fetchProducts = async () => {
+  const res = await axios.get('/api/products');
+  return res.data;
+};
+
+export const createProduct = async (data) => {
+  const res = await axios.post('/api/products', data);
+  return res.data;
+};
